60eea1d3058ed4e335d9bb37bbe72f375d957deb
CHARITAH <102732524+CHARITAH@users.noreply.github.com>
2023-04-06 11:54:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    rect(30, 20, 55, 55, 20);
+
+
+  }
+
+  function take_snapshot(){    
+    save('student_name.png');
+  }
